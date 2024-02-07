b7506d13509a80649e85bda3313bd87e8bcdf28a
maikoncorrea <maikonacorrea@gmail.com>
2024-02-06 22:09:01 -0300

    added to schema email and password in models/user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
+const isEmail = require('../node_modules/validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Formato do e-mail é inválido!',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
   name: {
     type: String,
     required: [true, 'O campo name precisa ser preenchido'],
