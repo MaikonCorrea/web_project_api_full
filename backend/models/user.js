f36e425bc11043a64e78c8a862fd3e364808801d
maikoncorrea <maikonacorrea@gmail.com>
2024-02-08 12:33:38 -0300

    removed required from name and about / added default name/about/avatar in models/user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -18,18 +18,19 @@ const userSchema = new mongoose.Schema({
   },
   name: {
     type: String,
-    required: [true, 'O campo name precisa ser preenchido'],
+    default: 'Jacques Cousteau',
     minlength: [2, 'O nome deve ter pelo menos 2 caracteres'],
     maxlength: [30, 'O nome não pode ter mais de 30 caracteres'],
   },
   about: {
     type: String,
-    required: [true, 'O campo about precisa ser preenchido'],
+    default: 'Explorer',
     minlength: [2, 'O campo about deve ter pelo menos 2 caracteres'],
     maxlength: [30, 'O campo about não pode ter mais de 30 caracteres'],
   },
   avatar: {
     type: String,
+    default: 'https://practicum-content.s3.us-west-1.amazonaws.com/resources/moved_avatar_1604080799.jpg',
   },
 }, {
   versionKey: false,
