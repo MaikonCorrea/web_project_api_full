14427d15b093e37a687bc88d1ed96debde1faeb2
maikoncorrea <maikonacorrea@gmail.com>
2024-02-14 06:17:58 -0300

    added bodyParser and signin /signup in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const cardsRouter = require('./routes/cards');
-const { login, createUser } = require('./controllers/users');
+const {
+  login, createUser, updateProfile,
+} = require('./controllers/users');
 const auth = require('./middleware/auth');
 
 const { PORT = 3000 } = process.env;
@@ -10,14 +13,16 @@ const connectDatabase = require('./data/database');
 const app = express();
 connectDatabase();
 
-app.use(express.json());
-
-app.post('/signin', login);
-app.post('/signup', createUser);
-
+app.use(bodyParser.json());
 app.use(auth);
+app.post('/signin', login); // em andamento
+app.post('/signup', createUser); // funcionando
 
-app.use('/cards', cardsRouter);
+app.get('/cards', cardsRouter); // funcionando
+
+app.patch('/users/me', updateProfile); // precisa fazer ainda
+
+
 
 app.use('/', (req, res, next) => {
   res.status(404).json({ message: 'A solicitação não foi encontrada mesmo', status: 404 });
